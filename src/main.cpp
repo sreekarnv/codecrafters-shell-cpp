1fdae1c51d1713a5d7c1bc6f0c8c61aba277e7f5
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-07-08 17:53:59 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main() {
       std::string command;
       std::getline(std::cin, command);
 
-      // if (command == "exit") break;
+      if (command == "exit") break;
 
       std::cout << command << ": command not found" << std::endl;
   }
