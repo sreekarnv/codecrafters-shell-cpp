90469f62fb3bb79cf18411ca6073a47609a9469d
Sreekar Nutulapati <sreekarnv1@gmail.com>
2026-07-08 17:53:46 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,9 @@ int main() {
       std::cout << "$ ";
       std::string command;
       std::getline(std::cin, command);
+
+      // if (command == "exit") break;
+
       std::cout << command << ": command not found" << std::endl;
   }
 }
